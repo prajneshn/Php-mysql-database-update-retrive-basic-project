35a38a44841d305d09ad43c2e137ea115798e92c
PRAJNESH N <94782333+prajneshn@users.noreply.github.com>
2022-09-20 19:46:52 +0530

    final

--- next.php
+++ next.php
@@ -32,7 +32,7 @@ $database="intern";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form </title>
+    <title>Form submission and displaying result</title>
     <style>
         *{
             margin: 0;
